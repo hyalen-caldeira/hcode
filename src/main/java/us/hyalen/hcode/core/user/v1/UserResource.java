5f8b85703d1b5ecda10351a2f5c77c4bf59dfff3
Hyalen Moreira Caldeira <hyalen.moreira@gmail.com>
2018-07-31 21:48:25 -0700

    Implemented user code

--- src/main/java/us/hyalen/hcode/core/user/v1/UserResource.java
+++ src/main/java/us/hyalen/hcode/core/user/v1/UserResource.java
@@ -1,4 +1,29 @@
 package us.hyalen.hcode.core.user.v1;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
 public class UserResource {
+    public static final String MEDIA_TYPE = "app/us.hyalen.user.v1+json";
+
+    @NotNull
+    public Long userId;
+    @NotNull
+    @Size(max = 20)
+    public String firstName;
+    @NotNull
+    @Size(max = 20)
+    public String lastName;
+    @NotNull
+    @Size(max = 40)
+    public String email;
+    @NotNull
+    @Size(max = 15)
+    public String login;
+    @NotNull
+    @Size(max = 15)
+    public String password;
 }
--- src/main/java/us/hyalen/hcode/model/CompanyModel.java
+++ src/main/java/us/hyalen/hcode/model/CompanyModel.java
@@ -1,4 +1,33 @@
 package us.hyalen.hcode.model;
 
+import lombok.Data;
+import us.hyalen.hcode.core.company.v1.Company;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "COMPANY", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "companyName"
+        })
+})
+@Data
 public class CompanyModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 40)
+    private String companyName;
+
+    public CompanyModel() {}
+
+    public CompanyModel(Company company) {
+
+    }
 }
